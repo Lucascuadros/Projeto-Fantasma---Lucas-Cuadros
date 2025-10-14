d8e65f86e382555625c571b7e8639427841b62b5
Lucascuadros <lucascuadross1403@gmail.com>
2025-10-14 11:37:43 -0300

    Entrega 2

--- rdocs/entrega_2.R
+++ rdocs/entrega_2.R
@@ -22,3 +22,29 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+#Lendo a página do Excel
+infos_clientes <- read_excel("relatorio_old_town_road.xlsx", 
+                                      sheet = "infos_clientes")
+
+#Fazendo as tranformações necessárias
+infos_clientes$Weight_lbs <- round(infos_clientes$Weight_lbs * 0.45359237,2)
+infos_clientes<- infos_clientes %>%
+  rename(Peso_Kg = Weight_lbs)
+infos_clientes$Height_dm <- round(infos_clientes$Height_dm * 10,2)
+infos_clientes <- infos_clientes %>%
+  rename(Altura_cm = Height_dm)
+
+#Fazendo um gráfiico de dispersão
+Grafico_dispersao <- ggplot(infos_clientes) +
+  aes(x = Peso_Kg, y = Altura_cm) +
+  geom_point(colour = "#A11D21", size = 3) +
+  geom_smooth(method = "lm", se = FALSE, colour = "#003366", linewidth = 1) +
+  labs(
+    x = "Peso (Kg)",
+    y = "Altura (cm)"
+  ) +
+  theme_estat()
